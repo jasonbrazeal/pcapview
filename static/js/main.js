80ff990b5327787a0677ad55f11e0be38ba5327e
Jason Brazeal <jsonbrazeal@gmail.com>
2015-11-05 21:01:13 -0500

    added css spinner

--- static/js/main.js
+++ static/js/main.js
@@ -26,69 +26,33 @@ $(document).ready(function() {
   });
   dropZone.on('drop', function (e)
   {
-       $(this).css('border', '0');
-       e.stopPropagation();
-       e.preventDefault();
-       var files = e.originalEvent.dataTransfer.files;
-       if (files.length != 1) {
-        errMsg = "exactly one pcap file required";
-        handleError(errMsg)
-       } else {
-        var fd = new FormData();
-        fd.append("file", files[0]);
-        $.ajax({
-          url: "/ajax",
-          type: "POST",
-          data: fd,
-          processData: false,  // tell jQuery not to process the data
-          contentType: false   // tell jQuery not to set contentType
-        }).done(function(data) {
-          console.log(data);
-          alert( "success" );
-        }).fail(function(data) {
-          console.log(data);
-          alert( "error" );
-        });
-       }
-  });
-
-
-
-  // dropZone ondrop() {
-
-  // ajax()
-  // }
-
-  // upload form onsubmit() {
-
-
-  // ajax()
-  // }
-
-
-  // ajax() {
-
-
-  // }
-
-  // ajax success() {
-
-  // display graph(data)()
-  // }
-
-  // ajax error() {
-
-  // display error(data)()
-  // }
-
-
-  // display graph(data) {
-
-  // }
-
-  // display error(data) {
-
-  // }
-
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).css('border', '0');
+    $('<div class="progress"><div></div></div>').insertAfter('.container');
+    var files = e.originalEvent.dataTransfer.files;
+    if (files.length != 1) {
+     errMsg = "exactly one pcap file required";
+     handleError(errMsg)
+    } else {
+      var fd = new FormData();
+      fd.append("file", files[0]);
+      $.ajax({
+        url: "/ajax",
+        type: "POST",
+        data: fd,
+        processData: false,  // tell jQuery not to process the data
+        contentType: false   // tell jQuery not to set contentType
+      }).done(function(data) {
+        console.log(data);
+        alert( "success" );
+      }).fail(function(data) {
+        console.log(data);
+        alert( "error" );
+      }).always(function(data) {
+        $('.progress').remove();
+      });
+    }
+  }); /* on drop */
 
 }); /* document.ready */
